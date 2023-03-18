96fc7ef44731a2828e2fc6e36f14f2ae6b329d90
ganeshkpbhle <59686247+ganeshkpbhle@users.noreply.github.com>
2023-03-18 09:19:27 +0530

    Saving progress

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ window.onload = function () {
 function scrollFunction() {
 	if (document.documentElement.scrollTop > 30) {
 		document.getElementById("navbarExample").classList.add("top-nav-collapse");
-	} else if ( document.documentElement.scrollTop < 30 ) {
+	} else if (document.documentElement.scrollTop < 30) {
 		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
 	}
 }
@@ -29,7 +29,7 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 document.querySelector(".navbar-toggler").addEventListener("click", () => {
-  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
+	document.querySelector(".offcanvas-collapse").classList.toggle("open");
 });
 
 // Hover on desktop
@@ -39,11 +39,11 @@ function toggleDropdown(e) {
 
 	setTimeout(
 		function () {
-		const shouldOpen = _d.matches(":hover");
-		_m.classList.toggle("show", shouldOpen);
-		_d.classList.toggle("show", shouldOpen);
+			const shouldOpen = _d.matches(":hover");
+			_m.classList.toggle("show", shouldOpen);
+			_d.classList.toggle("show", shouldOpen);
 
-		_d.setAttribute("aria-expanded", shouldOpen);
+			_d.setAttribute("aria-expanded", shouldOpen);
 		},
 		e.type === "mouseleave" ? 300 : 0
 	);
@@ -52,7 +52,7 @@ function toggleDropdown(e) {
 // On hover
 const dropdownCheck = document.querySelector('.dropdown');
 
-if (dropdownCheck !== null) { 
+if (dropdownCheck !== null) {
 	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
 	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
 
@@ -60,12 +60,14 @@ if (dropdownCheck !== null) {
 	document.querySelector(".dropdown").addEventListener("click", (e) => {
 		const _d = e.target.closest(".dropdown");
 		let _m = document.querySelector(".dropdown-menu", _d);
-		if (_d.classList.contains("show")) {
-			_m.classList.remove("show");
-			_d.classList.remove("show");
-		} else {
-			_m.classList.add("show");
-			_d.classList.add("show");
+		if (_d?.classList != null) {
+			if (_d.classList.contains("show")) {
+				_m.classList.remove("show");
+				_d.classList.remove("show");
+			} else {
+				_m.classList.add("show");
+				_d.classList.add("show");
+			}
 		}
 	});
 }
@@ -127,19 +129,19 @@ var video = document.getElementById('video')
 var videoSrc
 
 var checkVideoSrc = document.querySelector('.video-btn');
-if (checkVideoSrc !== null) { 
-	videoBtn.addEventListener('click',function(e){
+if (checkVideoSrc !== null) {
+	videoBtn.addEventListener('click', function (e) {
 		videoSrc = videoBtn.getAttribute('data-bs-src')
 	})
 }
 
 var checkVideoModal = document.getElementById('videoModal');
-if (checkVideoModal !== null) { 
-	videoModal.addEventListener('shown.bs.modal',(e)=>{
+if (checkVideoModal !== null) {
+	videoModal.addEventListener('shown.bs.modal', (e) => {
 		video.setAttribute('src', videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0')
 	})
 
-	videoModal.addEventListener('hide.bs.modal',(e)=>{
+	videoModal.addEventListener('hide.bs.modal', (e) => {
 		video.setAttribute('src', videoSrc)
 	})
 }
@@ -163,3 +165,37 @@ function topFunction() {
 	document.body.scrollTop = 0; // for Safari
 	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 }
+
+// Get the dropdown button and content elements
+var dropdownBtn = document.getElementById("dropdown-btn");
+var dropdownContent = document.querySelector(".dropdown-menu");
+
+// When the button is clicked
+dropdownBtn.addEventListener("click", function () {
+	if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+		dropdownContent.style.display = "block";
+		// Change the button icon to an angle up
+		this.innerHTML = '<i class="fa-solid fa-angle-up">';
+	} else {
+		dropdownContent.style.display = "none";
+		// Change the button icon to an angle down
+		this.innerHTML = '<i class="fa-solid fa-angle-down">';
+	}
+	// Toggle the "active" class on the button
+	this.classList.toggle("active");
+});
+window.addEventListener("resize", function () {
+	if (window.innerWidth >= 992) {
+		// Change the button icon to an angle down
+		dropdownBtn.innerHTML = '<i class="fas fa-angle-down"></i>';
+		// Remove the "active" class from the button
+		dropdownBtn.classList.remove("active");
+	}
+	if(window.innerWidth >= 992 && (dropdownContent.style.display==="none"||dropdownContent.style.display==="block")){
+		dropdownContent.style.display="";
+		// Change the button icon to an angle down
+		dropdownBtn.innerHTML = '<i class="fas fa-angle-down"></i>';
+		// Remove the "active" class from the button
+		dropdownBtn.classList.remove("active");
+	}
+});
